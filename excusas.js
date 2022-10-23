da237d73b42c1fa7f6c687f44d07f11509bd4fa2
dani <danieljuhe@gmail.com>
2022-10-23 12:19:25 +0200

    subiendo archivo html

--- excusas.js
+++ excusas.js
@@ -4,10 +4,12 @@ var verb=['eat','pissed','crushed','broked'];
 var when=['before the class','right in time','when I finished','during my lunch','while I was praying'];
 var thing=['my homework' , 'the keys' , 'the car'];
 
+
 const randint =(top)=>{
    const index = Math.floor(Math.random()*top)
    if (index === top) return top-1
    return index
 }
-
-console.log ((person [randint(person.length)]),(verb [randint(verb.length)]),(thing [randint(thing.length)]),(when[randint(when.length)]));
+window.onload = function(){
+console.log((person [randint(person.length)]),(verb [randint(verb.length)]),(thing [randint(thing.length)]),(when[randint(when.length)]));
+}
